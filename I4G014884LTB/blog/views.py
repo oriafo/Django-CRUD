63322311b6489abc24e7c7cf18df5e717355feca
oriafo <abrahamibharunujele@gmail.com>
2022-06-24 09:22:58 -0700

    Django CRUD

---
+++ I4G014884LTB/blog/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.views import generic        #importing generic class so we can use the ListView method.
+
+# Create your views here.
+class PostListView(generic.ListView):
+    model = Post
+
+class PostCreateView(generic.CreateView):
+    model = Post
+    fields = “__all__”
+    success_url  = reverse_lazy(“blog:all”)
+
+class PostDetailView(generic.DetailView):
+    model = Post
+
+class PostUpdateView(generic.UpdateView):
+    model = Post
+    fields = “__all__”
+    success_url  = reverse_lazy(“blog:all”)
+
+class PostDeleteView(generic.UpdateView):
+    model = Post
+    fields = “__all__”
+    success_url  = reverse_lazy(“blog:all”)   
